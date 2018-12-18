50ed6a24518565d649589d55d5710ae6277e8c21
Alan M.E <alanmarquez@outlook.com>
2018-12-17 23:18:47 -0500

    added enumeration server events

---
+++ jnum-server-wrapper/lib/listener.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+    RESQUEST_LISTENER: 'Request_Listener',
+    CONTROLLER_LISTENER: 'Controller_Listener',
+    API_LISTENER: 'API_Listener',
+    FILTER_LISTENER: 'Filter_Listener' 
+}
