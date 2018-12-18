c367facd02c2686a87b6e7d980b5d1b7b53ab844
Alan M.E <alanmarquez@outlook.com>
2018-12-17 23:09:50 -0500

    added enumeration body parser HTTP

---
+++ jnum-server-wrapper/lib/body-parser.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+    URLENCODED: 'application/x-www-form-urlencoded',
+    MULTER: 'multipart/form-data',
+    JSON: 'json',
+    XML: 'xml', // not support
+    YML: 'yml' // not support
+}
